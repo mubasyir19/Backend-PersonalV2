198396c8f3d8699434ad31b77cc075c17bc6a011
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-02-04 15:05:28 +0700

    add(account): feature regsiter

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,19 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
+import { PrismaService } from 'src/prisma.service';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class AccountService {
-  create(createAccountDto: CreateAccountDto) {
-    return 'This action adds a new account';
+  constructor(private prisma: PrismaService) {}
+
+  async register(createAccountDto: CreateAccountDto) {
+    const checkUser = await this.findUser(createAccountDto.username);
+    if (checkUser) {
+      throw new ConflictException('username has been registered');
+    }
+
+    const newUser = await this.prisma.account.create({
+      data: {
+        ...createAccountDto,
+        password: await hash(createAccountDto.password, 10),
+      },
+    });
+
+    const { password, ...user } = newUser;
+
+    return user;
+  }
+
+  async findUser(username: string) {
+    try {
+      const user = await this.prisma.account.findFirst({
+        where: { username },
+      });
+      return user;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   findAll() {
     return `This action returns all account`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
+  async findOne(id: string) {
+    const user = await this.prisma.account.findUnique({
+      where: { id },
+    });
+
+    const { password, ...data } = user;
+    return data;
   }
 
   update(id: number, updateAccountDto: UpdateAccountDto) {
--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAccountDto {}
+import { IsString } from 'class-validator';
+
+export class CreateAccountDto {
+  @IsString()
+  fullname: string;
+
+  @IsString()
+  biography: string;
+
+  @IsString()
+  job: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  password: string;
+}
